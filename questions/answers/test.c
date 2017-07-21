e8f29dfb994a873004f69bf9075984f573c42faf
sayon <igorjirkov@gmail.com>
2017-07-21 17:50:58 +0200

    more answers

--- questions/answers/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {                                                                
-    printf( "%d\n", * ((int*) 0x12345) );                    
-    return 0;                                                                   
-}                   
